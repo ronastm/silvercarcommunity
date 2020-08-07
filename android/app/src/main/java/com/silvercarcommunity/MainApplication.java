74427e321a783f457a0dcac8149c86528edb324d
ronastm <47625760+ronastm@users.noreply.github.com>
2020-08-07 11:17:36 +0700

    Google Sign In

--- android/app/src/main/java/com/silvercarcommunity/MainApplication.java
+++ android/app/src/main/java/com/silvercarcommunity/MainApplication.java
@@ -4,13 +4,13 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
@@ -26,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+          //packages.add(new RNGoogleSigninPackage());
           return packages;
         }
 
--- src/screens/config.js
+++
@@ -1,3 +0,0 @@
-export default {
-    webClientId: '354997727389-q3ll1jjquo6eq5t1ab5abgp163mctn0r.apps.googleusercontent.com',
-  };
